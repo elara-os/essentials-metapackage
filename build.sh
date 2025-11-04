63d54d9eb0db2287c86c576b2210e6336a7c0378
Josh Mangiola <contact@joshmangiola.com>
2025-11-05 07:06:16 +1030

    Fixing naming

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 BUILD_VERSION=$1
 DEBIAN_DIST=$2
 
-FULL_VERSION=$essentials_VERSION-${BUILD_VERSION}+${DEBIAN_DIST}_amd64
+FULL_VERSION=${BUILD_VERSION}+${DEBIAN_DIST}_amd64
 docker build . -t essentials-$DEBIAN_DIST  --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION
   id="$(docker create essentials-$DEBIAN_DIST)"
   docker cp $id:/essentials_$FULL_VERSION.deb - > ./essentials_$FULL_VERSION.deb
